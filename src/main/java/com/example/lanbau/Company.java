d6433d19f2768524359b9247cf618b74f5e3aaad
Irvin <irvinln@gmail.com>
2021-08-07 18:50:27 +0800

    almost there

--- src/main/java/com/example/lanbau/Company.java
+++ src/main/java/com/example/lanbau/Company.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 public class Company {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
